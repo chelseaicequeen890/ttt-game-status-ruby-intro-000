2a4b473cd95d55aa6d50f22a17050f655345221a
Alicia Wu <ali.ciawu890@gmail.com>
2019-01-29 21:16:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || full?(board) == true || draw?(board) == true
+  if won?(board) || full?(board) || draw?(board)
     return true
   end
 end
